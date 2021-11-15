37c2c1ca68cc822505696136111af59cb9fffa01
Artyom Shilov <artyom148@gmail.com>
2021-11-15 20:21:50 +0300

    calculator is implemented

--- src/main/java/com/epam/tat/calculator/impl/Calculator.java
+++ src/main/java/com/epam/tat/calculator/impl/Calculator.java
@@ -1,32 +1,46 @@
 package com.epam.tat.calculator.impl;
 
 import com.epam.tat.calculator.ICalculator;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Calculator implements ICalculator {
 
     private int precision;
+    private RoundingMode roundingMode;
 
     public Calculator(int precision) {
         this.precision = precision;
+        this.roundingMode = RoundingMode.HALF_DOWN;
+    }
+
+    public Calculator(){
+        this.precision = 5;
+        this.roundingMode = RoundingMode.HALF_DOWN;
+    }
+
+    public Calculator(int precision, RoundingMode roundingMode){
+        this.precision = precision;
+        this.roundingMode = roundingMode;
     }
 
     @Override
     public double add(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        return BigDecimal.valueOf(a + b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double subtract(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        return BigDecimal.valueOf(a - b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double multiply(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        return BigDecimal.valueOf(a * b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double divide(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        return BigDecimal.valueOf(a / b).setScale(precision, roundingMode).doubleValue();
     }
 }
