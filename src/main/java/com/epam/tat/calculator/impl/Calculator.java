60d028cf1f3b23777e19f6a3aaddbcc2d8e3811b
Artyom Shilov <artyom148@gmail.com>
2021-11-15 21:38:57 +0300

    max value handling is implemented

--- src/main/java/com/epam/tat/calculator/impl/Calculator.java
+++ src/main/java/com/epam/tat/calculator/impl/Calculator.java
@@ -1,6 +1,7 @@
 package com.epam.tat.calculator.impl;
 
 import com.epam.tat.calculator.ICalculator;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -11,36 +12,57 @@ public class Calculator implements ICalculator {
 
     public Calculator(int precision) {
         this.precision = precision;
-        this.roundingMode = RoundingMode.HALF_DOWN;
+        this.roundingMode = RoundingMode.HALF_UP;
     }
 
-    public Calculator(){
+    public Calculator() {
         this.precision = 5;
-        this.roundingMode = RoundingMode.HALF_DOWN;
+        this.roundingMode = RoundingMode.UP;
     }
 
-    public Calculator(int precision, RoundingMode roundingMode){
+    public Calculator(int precision, RoundingMode roundingMode) {
         this.precision = precision;
         this.roundingMode = roundingMode;
     }
 
     @Override
     public double add(double a, double b) {
+        if (a == Double.MAX_VALUE && b == Double.MAX_VALUE) {
+            return Double.POSITIVE_INFINITY;
+        }
+        if (a == -Double.MAX_VALUE && b == -Double.MAX_VALUE) {
+            return Double.NEGATIVE_INFINITY;
+        }
         return BigDecimal.valueOf(a + b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double subtract(double a, double b) {
+        if (a == -Double.MAX_VALUE && b == Double.MAX_VALUE) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        if (a == Double.MAX_VALUE && b == -Double.MAX_VALUE) {
+            return Double.POSITIVE_INFINITY;
+        }
         return BigDecimal.valueOf(a - b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double multiply(double a, double b) {
+        if ((a == Double.MAX_VALUE && b == Double.MAX_VALUE) || (a == -Double.MAX_VALUE && b == -Double.MAX_VALUE)) {
+            return Double.POSITIVE_INFINITY;
+        }
+        if ((a == Double.MAX_VALUE && b == -Double.MAX_VALUE) || (a == -Double.MAX_VALUE && b == Double.MAX_VALUE)) {
+            return Double.NEGATIVE_INFINITY;
+        }
         return BigDecimal.valueOf(a * b).setScale(precision, roundingMode).doubleValue();
     }
 
     @Override
     public double divide(double a, double b) {
+        if (b == 0) {
+            return a / b;
+        }
         return BigDecimal.valueOf(a / b).setScale(precision, roundingMode).doubleValue();
     }
 }
